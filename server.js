cf179be2b5931b38fbce44b9d9e5ad56a62a335a
Josue Masis <jmasisal@gmail.com>
2016-04-11 16:00:10 -0600

    Arreglos del CSS
    Se realizan mejoras al CSS y se acomoda mejor cada elemento

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var ioSocket = require('socket.io').listen(3000);
+var usuariosOnLine = [];
 
 ioSocket.sockets.on("connection", iniciar);
 
 function iniciar(socket){
 	socket.on("datos_cliente", devolverDatos);
+	socket.on("nickname", cargarUsuarios);
+}
+
+function cargarUsuarios(user){
+	usuariosOnLine.push(user);
+	ioSocket.sockets.emit("listaUser", usuariosOnLine);
 }
 
 function devolverDatos(data){
